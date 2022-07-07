5c61b47b50ba483371152bbb0b8f4c2b11419ab3
dragoncommits <75799735+dragoncommits@users.noreply.github.com>
2022-07-07 14:30:22 -0400

    updated app name in django admin to be pages instead of django flatpages

--- betterFlatPages/apps.py
+++ betterFlatPages/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BetterflatpagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'betterFlatPages'
+    verbose_name = 'pages'
